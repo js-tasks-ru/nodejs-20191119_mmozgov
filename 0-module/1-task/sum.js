5743675150bb9d46c35783713e131c803b78853d
mozgovm <mozgovmihail@yandex.ru>
2019-11-20 23:17:35 +0300

    add solution for 0-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,12 @@
 function sum(a, b) {
-  /* ваш код */
+	let args = [].slice.call(arguments);
+	  args.forEach(el => {
+		  if (typeof el !== 'number') {
+			  throw new TypeError(`${el} is not a number`);
+		  }
+	  });
+	  let result = args.reduce((sum, current) => sum + current, 0);
+	  return result;
 }
 
 module.exports = sum;
